da9526077843e7587b0b33fa7e0d9eb898e7a86e
K0RE222 <160282348+K0RE222@users.noreply.github.com>
2025-04-08 13:59:19 +0000

    I did it i think

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 // Create a function that tests the post route handler [2 pts]
-
+async function funcPost(){
+const response = await fetch("/create-potion")
+const data = await response.json()
+console.log(data)
+}
 
 
 // Call the function [1 pt]
 // Show us the successful POST fetch [1 pt]
-
+funcPost()
